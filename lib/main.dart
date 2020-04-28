be67b1b839121c5cd50b8a248d9bc04e8cd96b0e
maobui <mao.buidinh@hotmail.com>
2020-04-28 17:38:08 +0700

    InheritedWidget.

--- lib/main.dart
+++ lib/main.dart
@@ -10,54 +10,68 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: InheritedCounter(child: MyHomePage(title: 'Flutter Demo Home Page')),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+class MyHomePage extends StatelessWidget {
   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        int counter = InheritedCounter.of(context).counter;
+        Function increment = InheritedCounter.of(context).increment;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Hello "),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => setState(() => increment()),
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
   }
+
+}
+
+class InheritedCounter extends InheritedWidget {
+  final Map _counter = { 'val': 0};
+  final Widget child;
+
+  InheritedCounter({this.child}): super(child: child);
+
+  increment() {
+    _counter['val'] ++;
+  }
+
+  get counter => _counter['val'];
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  static InheritedCounter of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<InheritedCounter>();
+
 }
