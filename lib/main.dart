7dafd4a0b33a07072ecfba8977564aa977e33de5
maobui <mao.buidinh@hotmail.com>
2020-05-11 11:24:43 +0700

    Use redux.

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final store = new Store<int>(counterReducer, initialState: 0);
     return MaterialApp(
       title: 'Flutter State Management',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(store: store, title: 'Flutter Demo Home Page Redux'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+class MyHomePage extends StatelessWidget {
+  final Store<int> store;
   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  MyHomePage({Key key, this.store, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return StoreProvider<int>(
+      store: store,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'You have pushed the button this many times:',
+                ),
+                StoreConnector<int, String>(
+                  converter: (store) => store.state.toString(),
+                  builder: (context, count) {
+                    return Text(
+                      '$count',
+                      style: Theme.of(context).textTheme.display1,
+                    );
+                  },
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          floatingActionButton: StoreConnector<int, VoidCallback>(
+            converter: (store) {
+              return () => store.dispatch(Actions.Increment);
+            },
+            builder: (context, callback) {
+              return FloatingActionButton(
+                onPressed: callback,
+                tooltip: 'Increment',
+                child: Icon(Icons.add),
+              ); // This trailing comma makes auto-formatting nicer for build methods.
+            },
+          )),
     );
   }
+}
+
+enum Actions { Increment }
+
+int counterReducer(int state, dynamic action) {
+  if (action == Actions.Increment) {
+    return state + 1;
+  }
+  return state;
 }
